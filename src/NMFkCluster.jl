8158a00b00f12fee2f4b55e1a6d4d6fd07d8b55c
monty <velimir.vesselinov@gmail.com>
2017-03-03 11:59:56 -0700

    work in progress

--- src/NMFkCluster.jl
+++ src/NMFkCluster.jl
@@ -1,9 +1,55 @@
 import Distances
 
 "Cluster NMFk solutions"
-function clustersolutions(H, nNMF)
-	nP = size(H, 1) # number of observations (components/transients)
-	nT = size(H, 2) # number of total number of sources to cluster
+function clustersolutions(H::Vector, clusterweights::Bool)
+	nNMF = length(H)
+	nc, nr = size(H[1])
+	nk = clusterweights ? nr : nc
+
+	centroids = H[1]
+	idx = Array(Int, nk, nNMF)
+
+	for clusterIt = 1:nNMF
+		for globalIterID = 1:nNMF
+			processesTaken = falses(nk)
+			centroidsTaken = falses(nk)
+			for currentProcessID = 1:nk
+				distMatrix = ones(nk, nk) + Inf
+				for processID = 1:nk
+					if !processesTaken[processID]
+						for centroidID = 1:nk
+							if !centroidsTaken[centroidID]
+								if clusterweights
+									distMatrix[processID, centroidID] = Distances.cosine_dist(H[globalIterID][:, processID], centroids[:, centroidID])
+								else
+									distMatrix[processID, centroidID] = Distances.cosine_dist(H[globalIterID][processID, :], centroids[centroidID, :])
+								end
+							end
+						end
+					end
+				end
+				minProcess, minCentroid = ind2sub(size(distMatrix), indmin(distMatrix))
+				processesTaken[minProcess] = true
+				centroidsTaken[minCentroid] = true
+				idx[minProcess, globalIterID] = minCentroid
+			end
+		end
+		centroids = zeros(size(H[1]))
+		for centroidID = 1:nk
+			for globalIterID = 1:nNMF
+				if clusterweights
+					centroids[:, centroidID] += H[globalIterID][:, findin(idx[:, globalIterID], centroidID)]
+				else
+					centroids[:, centroidID] += H[globalIterID][findin(idx[:, globalIterID], centroidID), :]
+				end
+			end
+		end
+		centroids ./= nNMF
+	end
+	return idx, centroids'
+end
+function clustersolutions(H::Matrix, nNMF::Interger)
+	nP, nT = size(H) # number of observations (components/transients), number of total number of sources to cluster
 	nk = convert(Int, nT / nNMF )
 
 	centroids = H[:, 1:nk]
@@ -11,10 +57,10 @@ function clustersolutions(H, nNMF)
 
 	for clusterIt = 1:nNMF
 		for globalIterID = 1:nNMF
-			processesTaken = falses(nk, 1)
-			centroidsTaken = falses(nk, 1)
+			processesTaken = falses(nk)
+			centroidsTaken = falses(nk)
 			for currentProcessID = 1:nk
-				distMatrix = ones(nk, nk) + 99
+				distMatrix = ones(nk, nk) + Inf
 				for processID = 1:nk
 					if !processesTaken[processID]
 						for centroidID = 1:nk
