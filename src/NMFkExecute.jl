3ae564ccd3b73de6e58f96d97858c401c5d2c850
monty <velimir.vesselinov@gmail.com>
2017-03-03 12:01:59 -0700

    work in progress

--- src/NMFkExecute.jl
+++ src/NMFkExecute.jl
@@ -13,8 +13,170 @@ include("NMFkMixMatch.jl")
 include("NMFkIpopt.jl")
 include("NMFkMatrix.jl")
 
-"Execute NMFk analysis (in parallel if processors are available)"
 function execute(X::Matrix, nk::Int, nNMF::Int; ipopt::Bool=false, ratios::Union{Void,Array{Float32, 2}}=nothing, ratioindices::Union{Array{Int, 1},Array{Int, 2}}=Array(Int, 0, 0), deltas::Matrix{Float32}=Array(Float32, 0, 0), deltaindices::Vector{Int}=Array(Int, 0), quiet::Bool=true, best::Bool=true, mixmatch::Bool=false, normalize::Bool=false, scale::Bool=false, mixtures::Bool=true, matchwaterdeltas::Bool=false, maxiter::Int=10000, tol::Float64=1.0e-19, regularizationweight::Float32=convert(Float32, 0), ratiosweight::Float32=convert(Float32, 1), weightinverse::Bool=false, clusterweights::Bool=true, transpose::Bool=false)
+	# ipopt=true is equivalent to mixmatch = true && mixtures = false
+	!quiet && info("NMFk analysis of $nNMF NMF runs assuming $nk sources ...")
+	indexnan = isnan(X)
+	if any(indexnan) && (!ipopt || !mixmatch)
+		warn("The analyzed matrix has missing entries; NMF multiplex algorithm cannot be used; Ipopt minimization will be performed")
+		ipopt = true
+	end
+	numobservations = length(vec(X[!indexnan]))
+	if !quiet
+		if mixmatch
+			if matchwaterdeltas
+				println("Using MixMatchDeltas ...")
+			else
+				println("Using MixMatch ...")
+			end
+		elseif ipopt
+			println("Using Ipopt ...")
+		else
+			println("Using NMF ...")
+		end
+	end
+	if transpose
+		nP = size(X, 2) # number of observation points
+		nC = size(X, 1) # number of observed components/transients
+	else
+		nP = size(X, 1) # number of observation points
+		nC = size(X, 2) # number of observed components/transients
+	end
+	nRC = sizeof(deltas) == 0 ? nC : nC + size(deltas, 2)
+	WBig = SharedArray(Float64, nP, nNMF * nk)
+	HBig = SharedArray(Float64, nNMF * nk, nRC)
+	objvalue = SharedArray(Float64, nNMF)
+	@sync @parallel for i = 1:nNMF
+		if mixmatch
+			if matchwaterdeltas
+				W, H, objvalue[i] = NMFk.mixmatchwaterdeltas(X, nk; random=true, maxiter=maxiter, regularizationweight=regularizationweight)
+			else
+				if sizeof(deltas) == 0
+					W, H, objvalue[i] = NMFk.mixmatchdata(X, nk; ratios=ratios, ratioindices=ratioindices, random=true, mixtures=mixtures, normalize=normalize, scale=scale, maxiter=maxiter, regularizationweight=regularizationweight, weightinverse=weightinverse, ratiosweight=ratiosweight, quiet=quiet)
+				else
+					W, Hconc, Hdeltas, objvalue[i] = NMFk.mixmatchdata(X, deltas, deltaindices, nk; random=true, normalize=normalize, scale=scale, maxiter=maxiter, regularizationweight=regularizationweight, weightinverse=weightinverse, ratiosweight=ratiosweight, quiet=quiet)
+					H = [Hconc Hdeltas]
+				end
+			end
+		elseif ipopt
+			W, H, objvalue[i] = NMFk.ipopt(X, nk; random=true, normalize=normalize, scale=scale, maxiter=maxiter, regularizationweight=regularizationweight, weightinverse=weightinverse, quiet=quiet)
+		else
+			if scale
+				Xn, Xmax = NMFk.scalematrix(X)
+				if transpose
+					nmf_result = NMF.nnmf(Xn', nk; alg=:alspgrad, init=:random, maxiter=maxiter, tol=tol)
+				else
+					nmf_result = NMF.nnmf(Xn, nk; alg=:alspgrad, init=:random, maxiter=maxiter, tol=tol)
+				end
+				W = nmf_result.W
+				H = nmf_result.H
+				if transpose
+					W = NMFk.descalematrix_col(W, Xmax)
+					E = X' - W * H
+				else
+					H = NMFk.descalematrix(H, Xmax)
+					E = X - W * H
+				end
+				objvalue[i] = sum(E.^2)
+			else
+				if transpose
+					nmf_result = NMF.nnmf(X', nk; alg=:alspgrad, init=:random, maxiter=maxiter, tol=tol)
+				else
+					nmf_result = NMF.nnmf(X, nk; alg=:alspgrad, init=:random, maxiter=maxiter, tol=tol)
+				end
+				W = nmf_result.W
+				H = nmf_result.H
+				objvalue[i] = nmf_result.objvalue
+			end
+		end
+		!quiet && println("$(i): Objective function = $(objvalue[i])")
+		nmfindex = nk * i
+		WBig[1:nP, nmfindex-(nk-1):nmfindex] = W
+		HBig[nmfindex-(nk-1):nmfindex, 1:nRC] = H
+	end
+	!quiet && println("Best objective function = $(minimum(objvalue))")
+	nmfindex = nk * indmin(objvalue)
+	Wbest = WBig[1:nP, nmfindex-(nk-1):nmfindex]
+	Hbest = HBig[nmfindex-(nk-1):nmfindex, 1:nRC]
+	minsilhouette = 1
+	if nk > 1
+		if clusterweights
+			clusterassignments, M = NMFk.clustersolutions(WBig, nNMF) # cluster based on the W
+		else
+			clusterassignments, M = NMFk.clustersolutions(HBig', nNMF) # cluster based on the sources
+		end
+		!quiet && info("Cluster assignments:")
+		!quiet && display(clusterassignments)
+		!quiet && info("Cluster centroids:")
+		!quiet && display(M)
+		Wa, Ha, clustersilhouettes = NMFk.finalize(WBig, HBig, nNMF, clusterassignments)
+		minsilhouette = minimum(clustersilhouettes)
+		!quiet && info("Silhouettes for each of the $nk sources:" )
+		!quiet && display(clustersilhouettes')
+		!quiet && println("Mean silhouette = ", mean(clustersilhouettes) )
+		!quiet && println("Min  silhouette = ", minimum(clustersilhouettes) )
+	else
+		Wa = mean(WBig, 2)
+		Ha = mean(HBig, 1)
+	end
+	if best
+		Wa = Wbest
+		Ha = Hbest
+	end
+	if sizeof(deltas) == 0
+		if transpose
+			E = X' - Wa * Ha
+		else
+			E = X - Wa * Ha
+		end
+		E[isnan(E)] = 0
+		phi_final = sum(E.^2)
+	else
+		Ha_conc = Ha[:,1:nC]
+		Ha_deltas = Ha[:,nC+1:end]
+		estdeltas = NMFk.computedeltas(Wa, Ha_conc, Ha_deltas, deltaindices)
+		if transpose
+			E = X' - Wa * Ha_conc
+		else
+			E = X - Wa * Ha_conc
+		end
+		E[isnan(E)] = 0
+		id = !isnan(deltas)
+		phi_final = sum(E.^2) + sum((deltas[id] .- estdeltas[id]).^2)
+	end
+	if !quiet && typeof(ratios) != Void
+		ratiosreconstruction = 0
+		for (j, c1, c2) in zip(1:length(ratioindices[1,:]), ratioindices[1,:], ratioindices[2,:])
+			for i = 1:nP
+				s1 = 0
+				s2 = 0
+				for k = 1:nk
+					s1 += Wa[i, k] * Ha[k, c1]
+					s2 += Wa[i, k] * Ha[k, c2]
+				end
+				ratiosreconstruction += ratiosweight * (s1/s2 - ratios[i, j])^2
+			end
+		end
+		println("Ratio reconstruction = $ratiosreconstruction")
+		phi_final += ratiosreconstruction
+	end
+	numparameters = *(collect(size(Wa))...) + *(collect(size(Ha))...)
+	if mixmatch && mixtures
+		numparameters -= size(Wa)[1]
+	end
+	# numparameters = numbuckets # this is wrong
+	# dof = numobservations - numparameters # this is correct, but we cannot use because we may get negative DoF
+	# dof = maximum(range) - numparameters + 1 # this is a hack to make the dof positive.
+	# dof = dof < 0 ? 0 : dof
+	# sml = dof + numobservations * (log(fitquality[numbuckets]/dof) / 2 + 1.837877)
+	# aic[numbuckets] = sml + 2 * numparameters
+	aic = 2 * numparameters + numobservations * log(phi_final/numobservations)
+	!quiet && println("Objective function = ", phi_final, " Max error = ", maximum(E), " Min error = ", minimum(E) )
+	return Wa, Ha, phi_final, minsilhouette, aic
+end
+
+"Execute NMFk analysis (in parallel if processors are available)"
+function execute_new(X::Matrix, nk::Int, nNMF::Int; ipopt::Bool=false, ratios::Union{Void,Array{Float32, 2}}=nothing, ratioindices::Union{Array{Int, 1},Array{Int, 2}}=Array(Int, 0, 0), deltas::Matrix{Float32}=Array(Float32, 0, 0), deltaindices::Vector{Int}=Array(Int, 0), quiet::Bool=true, best::Bool=true, mixmatch::Bool=false, normalize::Bool=false, scale::Bool=false, mixtures::Bool=true, matchwaterdeltas::Bool=false, maxiter::Int=10000, tol::Float64=1.0e-19, regularizationweight::Float32=convert(Float32, 0), ratiosweight::Float32=convert(Float32, 1), weightinverse::Bool=false, clusterweights::Bool=true, transpose::Bool=false)
 	# ipopt=true is equivalent to mixmatch = true && mixtures = false
 	!quiet && info("NMFk analysis of $nNMF NMF runs assuming $nk sources ...")
 	indexnan = isnan(X)
