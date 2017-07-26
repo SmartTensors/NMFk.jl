fe5951a45ed93fea0289b9bfee575df1db508aec
monty <velimir.vesselinov@gmail.com>
2017-07-26 17:53:07 -0600

    NMFkBootstrap.jl fix

--- src/NMFkBootstrap.jl
+++ src/NMFkBootstrap.jl
@@ -1,14 +1,14 @@
 """
 Randomize matrix by assigning a random vector to each column based on a multinomial distribution (with the original column vector as the mean)
 """
-function bootstrap(H::Array, scale::Number=0.1)
-	G = similar(H)
-	for i in 1:size(H, 2)
-		r = round(Int64, H[:, i])
+function bootstrap(X::Array, scale::Number=0.1, epsilon::Number=sqrt(eps()))
+	N = similar(X)
+	for i in 1:size(X, 2)
+		r = round(Int64, X[:, i])
 		n = sum(r)
 		p = r ./ n
-		X = Distributions.Multinomial(n, p)
-		G[:, i] = rand(X) .* scale
+		v = Distributions.Multinomial(n, p)
+		N[:, i] = float(max(rand(v) .* scale, epsilon))
 	end
-	G
+	return N
 end
