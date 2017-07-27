a82aed03d69b53d709a9cdb4ffb69ee1341f00ca
monty <velimir.vesselinov@gmail.com>
2017-07-27 07:12:48 -0600

    simple Multiplicative NMF added

---
+++ src/NMFkMultiplicative.jl
@@ -0,0 +1,55 @@
+function NMFmultiplicative(X::Array, k::Int; quiet::Bool=true, stopconv::Int=10000, maxiter::Int=1000000, seed::Int=-1)
+	if minimum(X) < 0
+		error("All matrix entries must be nonnegative")
+	end
+	if minimum(sum(X, 2)) == 0
+		error("All matrix entries in a row can be 0!")
+	end
+
+	if seed >= 0
+		srand(seed)
+	end
+
+	n, m = size(X)
+
+	consold = falses(m, m)
+	inc = 0
+
+	W = rand(n, k)
+	H = rand(k, m)
+
+	index = Array(Int, m)
+	for i=1:maxiter
+		# X1 = repmat(sum(W, 1)', 1, m)
+		H = H .* (W' * (X ./ (W * H))) ./ sum(W, 1)'
+		# X2 = repmat(sum(H, 2)', n, 1)
+		W = W .* ((X ./ (W * H)) * H') ./ sum(H, 2)'
+		if mod(i, 10) == 0
+			H = max(H, eps())
+			W = max(W, eps())
+			for q = 1:m
+				index[q] = indmax(H[:, q])
+			end
+			# sum(map(i->sum(index.==i).^2, 1:3))
+			cons = repmat(index, 1, m) .== repmat(index', m, 1)
+			consdiff = sum(cons .!= consold)
+
+			if consdiff == 0
+				inc += 1
+			else
+				inc = 0
+			end
+
+			if !quiet
+				@printf("\t%d\t%d\t%d\n", i, inc, consdiff)
+			end
+
+			if inc > stopconv
+				break
+			end
+
+			consold = cons
+		end
+	end
+	return W, H
+end
