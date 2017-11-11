3b9fabd8a7a95eb20512960353a4a7a415dc33cc
monty <velimir.vesselinov@gmail.com>
2017-11-10 17:35:19 -0700

    NMFkBootstrap fixed

--- src/NMFkBootstrap.jl
+++ src/NMFkBootstrap.jl
@@ -3,18 +3,17 @@ import Distributions
 """
 Randomize matrix by assigning a random vector to each column based on a multinomial distribution (with the original column vector as the mean)
 """
-function bootstrapping(X::Array, scale::Number=0.1, epsilon::Number=sqrt(eps()))
+function bootstrapping(X::Array, epsilon::Number=sqrt(eps()))
 	N = deepcopy(X)
-	bootstrapping!(N, scale, epsilon)
+	bootstrapping!(N, epsilon)
 	return N
 end
 
-function bootstrapping!(X::Array, scale::Number=0.1, epsilon::Number=sqrt(eps()))
+function bootstrapping!(X::Array, epsilon::Number=sqrt(eps()))
 	for i in 1:size(X, 2)
-		r = round.(Int64, X[:, i])
-		n = sum(r)
-		p = r ./ n
-		v = Distributions.Multinomial(n, p)
-		X[:, i] = float(max.(rand(v) .* scale, epsilon))
+		n = sum(X[:, i])
+		p = (X[:, i] ./ n)
+		v = Distributions.Multinomial(convert(Int64, round(n)), p)
+		X[:, i] = float(max.(rand(v), epsilon))
 	end
 end
