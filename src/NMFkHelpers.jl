7801bae1fec17f9f42945a28e889d35aed6e7a36
monty <velimir.vesselinov@gmail.com>
2019-06-17 09:37:05 -0600

    major fixes

--- src/NMFkHelpers.jl
+++ src/NMFkHelpers.jl
@@ -53,6 +53,14 @@ function cornan(x, y)
 	end
 end
 
+function hardencodelength(x::Vector{T}) where {T}
+	u = unique(x)
+	i = indexin(x, u)
+	inan = indexin(true, isnan.(u))[1]
+	d = inan != nothing ? length(u) - 1 : d = length(u)
+	return d
+end
+
 function hardencode(x::Vector{T}) where {T}
 	u = unique(x)
 	i = indexin(x, u)
@@ -75,6 +83,44 @@ function hardencode(x::Vector{T}) where {T}
 	return m
 end
 
+
 function hardencode(x::Matrix{T}) where {T}
 	hcat([hardencode(x[:,i]) for i = 1:size(x, 2)]...)
+end
+
+function gettypes(x::Matrix{T}, levels=[0.05,0.35]) where {T}
+	nlevels = length(levels)
+	nw = size(x, 1)
+	ns = size(x, 2)
+	s = zeros(T, nw, ns)
+	for i = 1:ns
+		for l = 1:nlevels
+			if l == nlevels
+				s[:,i] .+= (x[:,i] .>= levels[l]) .* l
+			else
+				s[:,i] .+= ((x[:,i] .>= levels[l]) .& (x[:,i] .< levels[l+1])) .* l
+			end
+		end
+	end
+	wcode = sum(hcat(map(j->s[:,j].*10^j, 1:ns)...); dims=2)
+	uniquewcode = unique(wcode)
+	nt = length(uniquewcode)
+	@info("Number of unique types is $nt)")
+	types = nt < 31 ? collect(range('A'; length=nt)) : ["T$i" for i=1:nt]
+	iwcode = indexin(wcode, uniquewcode)
+	map(i->types[i], iwcode)
+end
+
+function harddecode(x::Matrix, h::Matrix{T}) where {T}
+	na = size(x, 2)
+	d = [hardencodelength(x[:,i]) for i = 1:na]
+	ns = size(h, 1)
+	s = Matrix{T}(undef, ns, na)
+	c = 1
+	for i = 1:na
+		ce = c + d[i] - 1
+		s[:,i] = sum(h[:,c:ce]; dims=2)
+		c = ce + 1
+	end
+	return s
 end
