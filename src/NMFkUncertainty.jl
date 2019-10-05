8630f0c1220d4fdf4b87d38ca94619ac35c77aa5
monty <velimir.vesselinov@gmail.com>
2019-10-05 14:33:17 -0600

    Uncertainty

--- src/NMFkUncertainty.jl
+++ src/NMFkUncertainty.jl
@@ -1,5 +1,5 @@
 "Execute NMFk analysis for a given of number of signals multiple times"
-function uncertainty(X::AbstractArray{T,N}, nk::Integer, nreruns::Integer, nNMF::Integer=10; save::Bool=false, casefilename::AbstractString="", kw...) where {T, N}
+function uncertainty(X::AbstractArray{T,N}, nk::Integer, nreruns::Integer, nNMF::Integer=10; window::Integer=size(X, 1), maxwindow::Integer=window, save::Bool=false, casefilename::AbstractString="", kw...) where {T, N}
 	W = Vector{Array{T, N}}(undef, nreruns)
 	H = Vector{Array{T, 2}}(undef, nreruns)
 	fitquality = Vector{T}(undef, nreruns)
@@ -9,18 +9,41 @@ function uncertainty(X::AbstractArray{T,N}, nk::Integer, nreruns::Integer, nNMF:
 		@info("Rerun $(i) out of $(nreruns):")
 		if save == true
 			if casefilename == ""
-				casefilenamemod = "nmfk_uncertinty_$i"
+				casefilenamemod = "nmfk_uncertainty_$i"
 			else
 				casefilenamemod = casefilename * "_$i"
 			end
 		else
 			casefilenamemod = ""
 		end
-		W[i], H[i], fitquality[i], robustness[i], aic[i] = NMFk.execute(X, nk, nNMF; kw..., save=save, casefilename=casefilenamemod)
+		W[i], H[i], fitquality[i], robustness[i], aic[i] = NMFk.execute(X[1:window,:], nk, nNMF; kw..., save=save, casefilename=casefilenamemod)
 	end
-	@info("Results")
+	if window == size(X,1)
+		@info("Results")
+	else
+		@info("Results stage #1")
+	end
 	for i in 1:nreruns
-		println("Signals: $(@sprintf("%2d", i)) Fit: $(@sprintf("%12.7g", fitquality[i])) Silhouette: $(@sprintf("%12.7g", robustness[i])) AIC: $(@sprintf("%12.7g", aic[i]))")
+		println("Rerun: $(@sprintf("%5d", i)) Fit: $(@sprintf("%12.7g", fitquality[i])) Silhouette: $(@sprintf("%12.7g", robustness[i])) AIC: $(@sprintf("%12.7g", aic[i]))")
+	end
+	if window != size(X,1)
+		for i in 1:nreruns
+			@info("Rerun stage #2 $(i) out of $(nreruns):")
+			if save == true
+				if casefilename == ""
+					casefilenamemod = "nmfk_uncertainty2_$i"
+				else
+					casefilenamemod = casefilename * "_stage2_$i"
+				end
+			else
+				casefilenamemod = ""
+			end
+			W[i], H[i], fitquality[i], robustness[i], aic[i] = NMFk.execute(X[1:maxwindow,:], nk; Hinit=convert.(T, H[i]), Hfixed=true, kw..., save=save, casefilename=casefilenamemod)
+		end
+		@info("Results stage #2")
+		for i in 1:nreruns
+			println("Rerun: $(@sprintf("%5d", i)) Fit: $(@sprintf("%12.7g", fitquality[i])) Silhouette: $(@sprintf("%12.7g", robustness[i])) AIC: $(@sprintf("%12.7g", aic[i]))")
+		end
 	end
 	return W, H, fitquality, robustness, aic
 end
