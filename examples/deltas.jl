319368b7614b746ccfdb96ef0720b765d7f58642
monty <velimir.vesselinov@gmail.com>
2019-10-19 09:59:40 -0600

    many important fixes

--- examples/deltas.jl
+++ examples/deltas.jl
@@ -2,94 +2,87 @@ import NMFk
 import LinearAlgebra
 import Random
 
-nummixtures = 20
-numbuckets = 2
 Random.seed!(2015)
-mixer = convert(Array{Float32,2}, rand(nummixtures, numbuckets))
-for i = 1:nummixtures
-	mixer[i, :] /= sum(mixer[i, :])
+nWells = 20
+nSources = 2
+wellmixing = convert(Array{Float32,2}, rand(nWells, nSources))
+for i = 1:nWells
+	wellmixing[i, :] /= sum(wellmixing[i, :])
 end
-buckets = convert(Array{Float32,2}, [100 0 3 50; 5 10 20 6])
-bucketdeltas = convert(Array{Float32,2}, [-10 3 0; -4 5 7])
-@info("Bucket concentrations")
-display(buckets)
-@info("Bucket deltas")
-display(bucketdeltas)
+sourceconcentrations = convert(Array{Float32,2}, [100 0 3 50; 5 10 20 6])
+sourcedeltas = convert(Array{Float32,2}, [-10 3 0; -4 5 7])
+@info("Source concentrations")
+display(sourceconcentrations)
+@info("Source deltas")
+display(sourcedeltas)
 deltaindices = [1, 3, 4]
-concentrations = convert(Array{Float32,2}, mixer * buckets)
-# concentrations[1, 1] = NaN
-deltas = convert(Array{Float32,2}, NMFk.computedeltas(mixer, buckets, bucketdeltas, deltaindices))
+concentrations = convert(Array{Float32,2}, wellmixing * sourceconcentrations)
+deltas = convert(Array{Float32,2}, NMFk.computedeltas(wellmixing, sourceconcentrations, sourcedeltas, deltaindices))
 
-#=
 @info("Concentrations")
 display(concentrations)
 @info("Deltas")
 display(deltas)
-=#
 
-@info("*** No transformation ...")
+@info("NMFk without transformation ...")
 
-fitmixer, fitbuckets, fitbucketdeltas, fitquality = NMFk.mixmatchdeltas(concentrations, deltas, deltaindices, 2; random=true)
-fitdeltas = NMFk.computedeltas(fitmixer, fitbuckets, fitbucketdeltas, deltaindices)
+fitwellmixing, fitsourceconcentrations, fitsourcedeltas, fitquality = NMFk.mixmatchdeltas(concentrations, deltas, deltaindices, 2; random=true)
+fitdeltas = NMFk.computedeltas(fitwellmixing, fitsourceconcentrations, fitsourcedeltas, deltaindices)
 
-#=
-@info("Match concentrations")
-display(concentrations - fitmixer * fitbuckets)
-@info("Match deltas")
-display(deltas - fitdeltas)
-=#
+@info("Estimated concentrations")
+display(concentrations .- fitwellmixing * fitsourceconcentrations)
+@info("Estimated deltas")
+display(deltas .- fitdeltas)
 
-of_c = sum(collect(concentrations - fitmixer * fitbuckets).^2)
+of_c = sum(collect(concentrations - fitwellmixing * fitsourceconcentrations).^2)
 of_d = sum(collect(deltas - fitdeltas).^2)
 of_t = of_c + of_d
-println("of conc   = $of_c")
-println("of deltas = $of_d")
-println("of total  = $of_t (check $fitquality)")
-@info("Bucket concentrations")
-display(fitbuckets)
-@info("Bucket deltas")
-display(fitbucketdeltas)
+println("OF conc   = $of_c")
+println("OF deltas = $of_d")
+println("OF total  = $of_t (check $fitquality)")
+@info("Source concentrations")
+display(fitsourceconcentrations)
+@info("Source deltas")
+display(fitsourcedeltas)
 
-@info("*** Scaling ...")
+@info("NMFk with Scaling ...")
 
-fitmixer, fitbuckets, fitbucketdeltas, fitquality = NMFk.mixmatchdeltas(concentrations, deltas, deltaindices, 2; random=true, scale=true)
-fitdeltas = NMFk.computedeltas(fitmixer, fitbuckets, fitbucketdeltas, deltaindices)
+fitwellmixing, fitsourceconcentrations, fitsourcedeltas, fitquality = NMFk.mixmatchdeltas(concentrations, deltas, deltaindices, 2; random=true, scale=true)
+fitdeltas = NMFk.computedeltas(fitwellmixing, fitsourceconcentrations, fitsourcedeltas, deltaindices)
 
-#=
-@info("Match concentrations")
-display(concentrations - fitmixer * fitbuckets)
-@info("Match deltas")
-display(deltas - fitdeltas)
-=#
-of_c = sum(collect(concentrations - fitmixer * fitbuckets).^2)
+@info("Estimated concentrations")
+display(concentrations .- fitwellmixing * fitsourceconcentrations)
+@info("Estimated deltas")
+display(deltas .- fitdeltas)
+
+of_c = sum(collect(concentrations - fitwellmixing * fitsourceconcentrations).^2)
 of_d = sum(collect(deltas - fitdeltas).^2)
 of_t = of_c + of_d
-println("of conc   = $of_c")
-println("of deltas = $of_d")
-println("of total  = $of_t (check $fitquality)")
-@info("Bucket concentrations")
-display(fitbuckets)
-@info("Bucket deltas")
-display(fitbucketdeltas)
+println("OF conc   = $of_c")
+println("OF deltas = $of_d")
+println("OF total  = $of_t (check $fitquality)")
+@info("Source concentrations")
+display(fitsourceconcentrations)
+@info("Source deltas")
+display(fitsourcedeltas)
 
-@info("*** Normalization ...")
+@info("NMFk with Normalization ...")
 
-fitmixer, fitbuckets, fitbucketdeltas, fitquality = NMFk.mixmatchdeltas(concentrations, deltas, deltaindices, 2; random=true, normalize=true)
-fitdeltas = NMFk.computedeltas(fitmixer, fitbuckets, fitbucketdeltas, deltaindices)
+fitwellmixing, fitsourceconcentrations, fitsourcedeltas, fitquality = NMFk.mixmatchdeltas(concentrations, deltas, deltaindices, 2; random=true, normalize=true)
+fitdeltas = NMFk.computedeltas(fitwellmixing, fitsourceconcentrations, fitsourcedeltas, deltaindices)
 
-#=
-@info("Match concentrations")
-display(concentrations - fitmixer * fitbuckets)
-@info("Match deltas")
-display(deltas - fitdeltas)
-=#
-of_c = sum(collect(concentrations - fitmixer * fitbuckets).^2)
-of_d = sum(collect(deltas - fitdeltas).^2)
+@info("Estimated concentrations")
+display(concentrations .- fitwellmixing * fitsourceconcentrations)
+@info("Estimated deltas")
+display(deltas .- fitdeltas)
+
+of_c = sum(collect(concentrations .- fitwellmixing * fitsourceconcentrations).^2)
+of_d = sum(collect(deltas .- fitdeltas).^2)
 of_t = of_c + of_d
-println("of conc   = $of_c")
-println("of deltas = $of_d")
-println("of total  = $of_t (check $fitquality)")
-@info("Bucket concentrations")
-display(fitbuckets)
-@info("Bucket deltas")
-display(fitbucketdeltas)
+println("OF conc   = $of_c")
+println("OF deltas = $of_d")
+println("OF total  = $of_t (check $fitquality)")
+@info("Source concentrations")
+display(fitsourceconcentrations)
+@info("Source deltas")
+display(fitsourcedeltas)
