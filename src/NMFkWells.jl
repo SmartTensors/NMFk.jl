80b2251d19ce81eaec947f7efb475f7e2c291301
monty <velimir.vesselinov@gmail.com>
2019-10-29 20:50:46 -0600

    Arp

--- src/NMFkWells.jl
+++ src/NMFkWells.jl
@@ -1,23 +1,23 @@
 """
-Aprs' relationship
+Arp's relationship
 """
-function arps(t::Union{Number,AbstractVector}, q0::Number, D::Number, b::Number)
+function arp(t::Union{Number,AbstractVector}, q0::Number, D::Number, b::Number)
 	q = q0 .* (1 .+ b * D .* t) .^ (1. / b)
 	return q
 end
 
 """
-Aprs' exponential relationship (b=0)
+Arp's exponential relationship (b=0)
 """
-function arps_exponential(t::Union{Number,AbstractVector}, q0::Number, D::Number)
+function arp_exponential(t::Union{Number,AbstractVector}, q0::Number, D::Number)
 	q = q0 .* exp.(-D .* t)
 	return q
 end
 
 """
-Aprs' harmonic relationship (b=1)
+Arp's harmonic relationship (b=1)
 """
-function arps_harmonic(t::Union{Number,AbstractVector}, q0::Number, D::Number)
+function arp_harmonic(t::Union{Number,AbstractVector}, q0::Number, D::Number)
 	q = q0 .* (1 .+ D .* t)
 	return q
 end
