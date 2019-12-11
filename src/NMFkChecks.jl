0bbf1d080d79003f878596964853bf855aa9ce1c
monty <velimir.vesselinov@gmail.com>
2019-12-10 19:15:04 -0800

    checkarray

--- src/NMFkChecks.jl
+++ src/NMFkChecks.jl
@@ -3,36 +3,36 @@ function checkarray(X::Array{T,N}, cutoff::Integer=0; func::Function=i->i>0, fun
 	for d = 1:N
 		@info("Dimension $d ...")
 		dd = size(X, d)
-		l = Array{Int64}(undef, dd)
-		e = Array{Int64}(undef, 0)
+		record_length = Array{Int64}(undef, dd)
+		bad_indices = Array{Int64}(undef, 0)
 		for i = 1:dd
 			nt = ntuple(k->(k == d ? i : Colon()), N)
 			firstentry = Base.findfirst(funcfirst.(X[nt...]))
 			if firstentry != nothing
 				lastentry = findlast(funclast.(X[nt...]))
 				if lastentry != nothing
-					l[i] = lastentry - firstentry + 1
+					record_length[i] = lastentry - firstentry + 1
 				else
-					l[i] = 0
+					record_length[i] = 0
 				end
 			else
-				l[i] = 0
+				record_length[i] = 0
 			end
-			if l[i] <= cutoff
-				push!(e, i)
+			if record_length[i] <= cutoff
+				push!(bad_indices, i)
 			end
 		end
-		if length(e) > 0
-			@info "Bad indices in dimension $d: $e"
+		if length(bad_indices) > 0
+			@info "Bad indices in dimension $d: $bad_indices"
 		else
 			@info "No bad indices in dimension $d!"
 		end
-		ir = sortperm(l)
-		@show l[ir][1:15]
-		@show l[ir][end-15:end]
-		@show maximum(l)
-		# md[d] = length(e) > 0 ? e[1] : 0
-		md[d] = maximum(l)
+		ir = sortperm(record_length)
+		@show record_length[ir][1:15]
+		@show record_length[ir][end-15:end]
+		@show maximum(record_length)
+		# md[d] = length(bad_indices) > 0 ? bad_indices[1] : 0
+		md[d] = maximum(record_length)
 	end
 	return md
 end
@@ -44,7 +44,7 @@ checkarray_count(X::Array, func) = checkarrayentries(X, func; ecount=true)
 function checkarrayentries(X::Array{T,N}, func::Function=.!isnan; good::Bool=false, ecount::Bool=false) where {T, N}
 	local flag = true
 	for d = 1:N
-		badindex = Array{Int64}(undef, 0)
+		selected_indeces = Array{Int64}(undef, 0)
 		ecount && (acount = Array{Int64}(undef, 0))
 		for i = 1:size(X, d)
 			nt = ntuple(k->(k == d ? i : Colon()), N)
@@ -52,20 +52,20 @@ function checkarrayentries(X::Array{T,N}, func::Function=.!isnan; good::Bool=fal
 			ecount && (push!(acount, c))
 			flagi = c > 0
 			if good
-				flagi && push!(badindex, i)
+				flagi && push!(selected_indeces, i)
 			else
-				!flagi && push!(badindex, i)
+				!flagi && push!(selected_indeces, i)
 			end
 			flag = flag && flagi
 		end
 		if ecount
 			@info "Count in dimension $d: $acount"
 		else
-			if length(badindex) > 0
+			if length(selected_indeces) > 0
 				if good
-					@info "Good indices in dimension $d: $badindex"
+					@info "Good indices in dimension $d: $selected_indeces"
 				else
-					@info "Bad indices in dimension $d: $badindex"
+					@info "Bad indices in dimension $d: $selected_indeces"
 				end
 			else
 				if good
