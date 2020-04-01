fec83d4b42c062361641a237dbd84e79f444920b
monty <velimir.vesselinov@gmail.com>
2020-04-01 15:03:28 -0600

    r2matrix

--- src/NMFkPlot.jl
+++ src/NMFkPlot.jl
@@ -528,13 +528,10 @@ end
 
 function r2matrix(X::AbstractArray, Y::AbstractArray; kw...)
 	D = Array{Float64}(undef, size(X, 2), size(Y, 2))
-	r2 = Vector{Float64}(undef, size(X, 2))
 	for i = 1:size(Y, 2)
 		for j = 1:size(X, 2)
-			r2[j] = NMFk.r2(X[:,j], Y[:,i])
+			D[j,i] = NMFk.r2(X[:,j], Y[:,i])
 		end
-		D[:,i] = r2
-		is = sortperm(r2; rev=true)
 	end
 	NMFk.plotmatrix(D; kw..., key_position=:none)
 end
