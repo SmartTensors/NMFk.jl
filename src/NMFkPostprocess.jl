512b87ca526535aa34ff514fa200b3bd10e6c48f
monty <velimir.vesselinov@gmail.com>
2020-05-08 07:33:18 -0600

    info

--- src/NMFkPostprocess.jl
+++ src/NMFkPostprocess.jl
@@ -4,7 +4,7 @@ import PlotlyJS
 """
 cutoff::Number = .9, cutoff_s::Number = 0.95
 """
-function clusterresults(nkrange, W, H, robustness, locations, attributes; clusterattributes::Bool=true, loadassignements::Bool=true, sizeW::Integer=0, lon=nothing, lat=nothing, cutoff::Number=0, cutoff_s::Number=0, figuredir::AbstractString=".", resultdir::AbstractString=".", casefilenameW::AbstractString="attributes", casefilenameH::AbstractString="locations", locationtypes=[], attributetypes=[], locationcolors=NMFk.colors, attributecolors=NMFk.colors, background_color="black")
+function clusterresults(nkrange, W, H, robustness, locations, attributes; clusterattributes::Bool=true, loadassignements::Bool=true, sizeW::Integer=0, lon=nothing, lat=nothing, cutoff::Number=0, cutoff_s::Number=0, figuredir::AbstractString=".", resultdir::AbstractString=".", casefilenameW::AbstractString="attributes", casefilenameH::AbstractString="locations", locationtypes=[], attributetypes=[], locationcolors=NMFk.colors, attributecolors=NMFk.colors, background_color="black", plotlabelH::Bool=true, plotlabelW::Bool=true)
 	if length(locationtypes) > 0
 		if locationcolors == NMFk.colors
 			locationcolors = Vector{String}(undef, length(locationtypes))
@@ -30,7 +30,7 @@ function clusterresults(nkrange, W, H, robustness, locations, attributes; cluste
 		@assert length(attributes) == size(W[k], 1)
 		@info("Number of signals: $k")
 
-		@info("Locations (signals=$k)")
+		@info("$(uppercasefirst(casefilenameH)) (signals=$k)")
 		recursivemkdir(resultdir; filename=false)
 		DelimitedFiles.writedlm("$resultdir/Hmatrix-$(k).csv", H[k], ',')
 		if cutoff > 0
@@ -63,18 +63,18 @@ function clusterresults(nkrange, W, H, robustness, locations, attributes; cluste
 		NMFk.plotmatrix(permutedims(H[k]) ./ maximum(H[k]); filename="$figuredir/$(casefilenameH)-$(k).png", vsize=12Compose.inch, xticks=["S$i" for i=1:k], yticks=["$(locations[i]) $(c[i])" for i=1:length(c)], colorkey=false)
 		NMFk.plotmatrix((permutedims(H[k]) ./ maximum(H[k]))[cs,is]; filename="$figuredir/$(casefilenameH)-sorted-$(k).png", xticks=cmap[is], yticks=["$(locations[cs][i]) $(c[cs][i])" for i=1:length(c)], colorkey=false)
 		NMFk.plotmatrix(permutedims((H[k] ./ sum(H[k]; dims=2)))[cs,is]; filename="$figuredir/$(casefilenameH)-sorted-sumrows-$(k).png", xticks=cmap[is], yticks=["$(locationnametype[cs][i]) $(c[cs][i])" for i=1:length(c)], colorkey=false)
-		NMFk.biplots(permutedims(H[k])[cs,is], locations[cs], cmap; filename="$figuredir/$(casefilenameH)-biplots-$(k).pdf", background_color=background_color, types=c[cs])
-		length(locationtypes) > 0 && NMFk.biplots(permutedims(H[k])[cs,is], locations[cs], cmap; filename="$figuredir/$(casefilenameH)-biplots-type-$(k).pdf", background_color=background_color, colors=locationcolors[cs])
+		NMFk.biplots(permutedims(H[k])[cs,is], locations[cs], cmap; filename="$figuredir/$(casefilenameH)-biplots-$(k).pdf", background_color=background_color, types=c[cs], plotlabel=plotlabelH)
+		length(locationtypes) > 0 && NMFk.biplots(permutedims(H[k])[cs,is], locations[cs], cmap; filename="$figuredir/$(casefilenameH)-biplots-type-$(k).pdf", background_color=background_color, colors=locationcolors[cs], plotlabel=plotlabelH)
 		if lon != nothing && lat != nothing
 			p = PlotlyJS.plot(NMFk.plot_wells(lon, lat, c), Plotly.Layout(title="Clusters: $k"))
 			PlotlyJS.savehtml(p, "$figuredir/clusters-$(k).html", :remote)
 			lonlat= [lon lat]
-		else
-			lonlat = Vector{Char}(undef, size(H[k], 2))
-			lonlat .= ' '
 		end
-		DelimitedFiles.writedlm("$resultdir/$(casefilenameH)-$(k).csv", [locations lonlat permutedims(H[k] ./ maximum(H[k])) c], ',')
-
+		if lon != nothing && lat != nothing
+			DelimitedFiles.writedlm("$resultdir/$(casefilenameH)-$(k).csv", [locations lonlat permutedims(H[k] ./ maximum(H[k])) c], ',')
+		else
+			DelimitedFiles.writedlm("$resultdir/$(casefilenameH)-$(k).csv", [locations permutedims(H[k] ./ maximum(H[k])) c], ',')
+		end
 		if clusterattributes
 			Wa = W[k]
 			if sizeW > 1
@@ -88,7 +88,7 @@ function clusterresults(nkrange, W, H, robustness, locations, attributes; cluste
 					i2 += sizeW
  				end
 			end
-			@info("Attributes (signals=$k)")
+			@info("$(uppercasefirst(casefilenameW)) (signals=$k)")
 			DelimitedFiles.writedlm("$resultdir/Wmatrix-$(k).csv", Wa, ',')
 			if cutoff > 0
 				ia = (Wa ./ maximum(Wa; dims=1)) .> cutoff
@@ -115,11 +115,11 @@ function clusterresults(nkrange, W, H, robustness, locations, attributes; cluste
 			end
 			cs = sortperm(cnew)
 			if any(cassgined .== 0)
-				@warn "Attributes not assigned to any cluster:"
+				@warn "$(uppercasefirst(casefilenameW)) not assigned to any cluster:"
 				display(attributes[cassgined .== 0])
 			end
 			if any(cassgined .> 1)
-				@warn "Attributes assigned to more than luster:"
+				@warn "$(uppercasefirst(casefilenameW)) assigned to more than luster:"
 				display([attributes[cassgined .> 1] cassgined[cassgined .> 1]])
 			end
 			io = open("$resultdir/$(casefilenameW)-groups-$(k).txt", "w")
@@ -145,8 +145,8 @@ function clusterresults(nkrange, W, H, robustness, locations, attributes; cluste
 			# NMFk.plotmatrix(Wa./sum(Wa; dims=1); filename="$figuredir/$(casefilenameW)-sum-$(k).png", xticks=["S$i" for i=1:k], yticks=["$(attributes[i]) $(c[i])" for i=1:length(cols)], colorkey=false)
 			# NMFk.plotmatrix((Wa./sum(Wa; dims=1))[cs,:]; filename="$figuredir/$(casefilenameW)-sum2-$(k).png", xticks=["S$i" for i=1:k], yticks=["$(attributes[cs][i]) $(c[cs][i])" for i=1:length(cols)], colorkey=false)
 			NMFk.plotmatrix((Wa ./ sum(Wa; dims=1))[cs,is]; filename="$figuredir/$(casefilenameW)-sorted-sumrows-$(k).png", xticks=cmap[is], yticks=["$(attributes[cs][i]) $(cnew[cs][i])" for i=1:length(c)], colorkey=false)
-			NMFk.biplots(Wa[cs,is], attributes[cs], cmap; filename="$figuredir/$(casefilenameW)-biplots-$(k).pdf", background_color=background_color, types=cnew[cs])
-			length(attributetypes) > 0 && NMFk.biplots(Wa[cs,is], attributes[cs], cmap; filename="$figuredir/$(casefilenameW)-biplots-type-$(k).pdf", background_color=background_color, colors=attributecolors[cs])
+			NMFk.biplots(Wa[cs,is], attributes[cs], cmap; filename="$figuredir/$(casefilenameW)-biplots-$(k).pdf", background_color=background_color, types=cnew[cs], plotlabel=plotlabelW)
+			length(attributetypes) > 0 && NMFk.biplots(Wa[cs,is], attributes[cs], cmap; filename="$figuredir/$(casefilenameW)-biplots-type-$(k).pdf", background_color=background_color, colors=attributecolors[cs], plotlabel=plotlabelW)
 			DelimitedFiles.writedlm("$resultdir/$(casefilenameW)-$(k).csv", [attributes (Wa ./ maximum(Wa))	 c], ',')
 
 			NMFk.biplots([Wa ./ maximum(Wa); permutedims(H[k] ./ maximum(H[k]))], [attributes; fill("", length(locations))], collect(1:k); filename="$figuredir/all-biplots-$(k).pdf", background_color=background_color, colors=[fill("gray", length(attributes)); locationcolors])
@@ -168,9 +168,15 @@ function clusterresults(nkrange, W, H, robustness, locations, attributes; cluste
 				table2 = hcat(table2, map(i->attributesl[Xekm[:,i]], 1:length(locations)))
 				table3 = hcat(table3, map(i->sum(Xekm[:,i]), 1:length(locations)))
 			end
-			DelimitedFiles.writedlm("$resultdir/$(casefilenameW)-$(k)-table_max.csv", [lonlat table], ',')
-			DelimitedFiles.writedlm("$resultdir/$(casefilenameW)-$(k)-table_$(cutoff_s).csv", [lonlat table2], ';')
-			DelimitedFiles.writedlm("$resultdir/$(casefilenameW)-$(k)-table_count_$(cutoff_s).csv", [lonlat table3], ',')
+			if lon != nothing && lat != nothing
+				DelimitedFiles.writedlm("$resultdir/$(casefilenameW)-$(k)-table_max.csv", [lonlat table], ',')
+				DelimitedFiles.writedlm("$resultdir/$(casefilenameW)-$(k)-table_$(cutoff_s).csv", [lonlat table2], ';')
+				DelimitedFiles.writedlm("$resultdir/$(casefilenameW)-$(k)-table_count_$(cutoff_s).csv", [lonlat table3], ',')
+			else
+				DelimitedFiles.writedlm("$resultdir/$(casefilenameW)-$(k)-table_max.csv", table, ',')
+				DelimitedFiles.writedlm("$resultdir/$(casefilenameW)-$(k)-table_$(cutoff_s).csv", table2, ';')
+				DelimitedFiles.writedlm("$resultdir/$(casefilenameW)-$(k)-table_count_$(cutoff_s).csv", table3, ',')
+			end
 			local table = attributesl
 			local table2 = attributesl
 			local table3 = attributesl
