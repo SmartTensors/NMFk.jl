2e75b0ad0ce65080ade9764270d05461ede41606
monty <velimir.vesselinov@gmail.com>
2020-12-15 11:43:10 -0700

    Docker

--- docker/setup.jl
+++ docker/setup.jl
@@ -1,6 +1,18 @@
 import Pkg
 Pkg.add("NMFk")
 Pkg.add("Mads")
+Pkg.add("Revise")
+Pkg.add("Flux")
+Pkg.add("Compose")
+Pkg.add("Cairo")
+Pkg.add("Fontconfig")
+Pkg.add("Gadfly")
 
 import NMFk
 import Mads
+import Revise
+import Flux
+import Compose
+import Cairo
+import Fontconfig
+import Gadfly
