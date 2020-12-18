96aef8d8112150194bd66fa7bf9774719656cd66
monty <velimir.vesselinov@gmail.com>
2020-12-18 08:38:02 -0700

    Docker

--- docker/setup.jl
+++ docker/setup.jl
@@ -16,3 +16,20 @@ import Compose
 import Cairo
 import Fontconfig
 import Gadfly
+
+NMFk.test()
+
+a = rand(20)
+b = rand(20)
+W = [a b]
+H = [.1 1 0 0 .1; 0 0 .1 .5 .2]
+X = W * H
+X = [a a*10 b b*5 a+b*2]
+
+nkrange = 2:3
+
+W, H, fitquality, robustness, aic = NMFk.execute(X, nkrange; resultdir="NMFk-test-results", load=true)
+resultdirpost = "NMFk-test-results/postprocessing"
+figuredirpost = "NMFk-test-results/postprocessing"
+NMFk.plot_feature_selecton(nkrange, fitquality, robustness; figuredir=figuredirpost)
+NMFk.clusterresults(NMFk.getks(nkrange, robustness[nkrange]), W, H, string.(collect(1:20)), string.(collect(1:5)); resultdir=resultdirpost, figuredir=figuredirpost, Wcasefilename="times", Hcasefilename="attributes", biplotcolor=:WH, sortmag=false, biplotlabel=:H, point_size_nolabel=2Gadfly.pt, point_size_label=4Gadfly.pt)
