4908e9bb47701934504967547d935279e7e54f92
monty <velimir.vesselinov@gmail.com>
2021-08-09 09:26:36 -0600

    notebooks

--- src/NMFkNotebooks.jl
+++ src/NMFkNotebooks.jl
@@ -1,3 +1,4 @@
-notebook(a..., k...) = Mads.notebook(a...; dir=NMFk.dir, k...)
-notebooks(a..., k...) = Mads.notebooks(a...; dir=NMFk.dir, k...)
-process_notebook(a..., k...) = Mads.process_notebook(a...; dir=NMFk.dir, k...)
+notebook(a...; k...) = Mads.notebook(a...; dir=NMFk.dir, k...)
+notebooks(a...; k...) = Mads.notebooks(a...; dir=NMFk.dir, k...)
+notebookscript(a...; k...) = Mads.notebookscript(a...; dir=NMFk.dir, k...)
+process_notebook(a...; k...) = Mads.process_notebook(a...; dir=NMFk.dir, k...)
