bc44cbea5a070400490377aeb73ee40af7311de7
monty <velimir.vesselinov@gmail.com>
2024-04-21 18:49:02 -0600

    Number of bins:

--- src/NMFkPreprocess.jl
+++ src/NMFkPreprocess.jl
@@ -181,6 +181,7 @@ function griddata(x::AbstractVector, y::AbstractVector; stepvalue=nothing, nbins
 	end
 	ix, xbins, gxmin, gxmax = NMFk.indicize(x; rev=xrev, nbins=xnbins, minvalue=xminvalue, maxvalue=xmaxvalue, stepvalue=xstepvalue, granulate=granulate, quiet=quiet)
 	iy, ybins, gymin, gymax = NMFk.indicize(y; rev=yrev, nbins=ynbins, minvalue=yminvalue, maxvalue=ymaxvalue, stepvalue=ystepvalue, granulate=granulate, quiet=quiet)
+	@info("Number of bins: $(xbins) $(ybins)")
 	return range(gxmin; stop=gxmax, length=xbins), range(gymin; stop=gymax, length=ybins)
 end
 
